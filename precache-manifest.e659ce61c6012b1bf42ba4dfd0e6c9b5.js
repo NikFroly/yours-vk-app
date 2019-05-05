d0b47fc815b46786dfd9efee0e55859f45e76bfd
NikFroly <NikFroly@gmail.com>
2019-05-05 22:46:38 +0700

    Updates

---
+++ precache-manifest.e659ce61c6012b1bf42ba4dfd0e6c9b5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "638942575967cdfba99e",
+    "url": "/yours-vk-app/static/js/runtime~main.63894257.js"
+  },
+  {
+    "revision": "b44a87bb2c58180d52f5",
+    "url": "/yours-vk-app/static/js/main.b44a87bb.chunk.js"
+  },
+  {
+    "revision": "8af17f961c68428d7386",
+    "url": "/yours-vk-app/static/js/2.8af17f96.chunk.js"
+  },
+  {
+    "revision": "8af17f961c68428d7386",
+    "url": "/yours-vk-app/static/css/2.40f4a263.chunk.css"
+  },
+  {
+    "revision": "3b4ed4633fae9860c2950c1ec4c6cc5b",
+    "url": "/yours-vk-app/index.html"
+  }
+];
